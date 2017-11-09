f728e389ee6a27e8a280f322af1f0abc3c377a16
VictorIranzo <viranzo96@gmail.com>
2017-11-09 23:50:50 +0100

    Renombradas clases y creado Run Application

---
+++ IEI-SegundaEntrega/src/app/ControllerViewApp.java
@@ -0,0 +1,51 @@
+package app;
+
+import java.awt.event.ActionEvent;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+
+public class ControllerViewApp {
+
+    @FXML
+    private ComboBox<?> comboArticulo;
+
+    @FXML
+    private CheckBox checkMediaMarkt;
+
+    @FXML
+    private CheckBox checkFnac;
+
+    @FXML
+    private Button btnBuscar;
+
+    @FXML
+    private CheckBox chTaurus;
+
+    @FXML
+    private CheckBox chDelongui;
+
+    @FXML
+    private CheckBox chBosch;
+
+    @FXML
+    private CheckBox chPhilips;
+
+    @FXML
+    private CheckBox chJata;
+
+    @FXML
+    private CheckBox chKrups;
+
+    @FXML
+    private CheckBox chJura;
+
+    @FXML
+    public void click_Buscar(Event event) {
+
+    }
+
+}
