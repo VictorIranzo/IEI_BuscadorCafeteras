b514d6339c82c6f875e21fa62f3bc16627c8c62e
VictorIranzo <viranzo96@gmail.com>
2017-11-11 23:57:14 +0100

    Selección de la marca en MediaMarkt driver. TODO a implementar.

--- IEI-SegundaEntrega/src/automationFramework/MediaMarktDriver.java
+++ IEI-SegundaEntrega/src/automationFramework/MediaMarktDriver.java
@@ -2,24 +2,52 @@ package automationFramework;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import app.Cafetera;
 
 public class MediaMarktDriver {
 	private static String urlConnection = "http://tiendas.mediamarkt.es/cafeteras-cafe";
 	private static HashMap<String, WebElement> categoriasWebElements;
+	private static HashMap<String, WebElement> marcasWebElements;
 	private static WebDriver driver;
 	
 	public static List<Cafetera> Search(String articulo, List<String> marcas) {		
 		categoriasWebElements.get(articulo).click();
+		obtenerMarcasArticulo();
+		
+		for(String marca : marcas) 
+		{
+			if(marcasWebElements.containsKey(marca)) {
+				WebDriverWait waiting = new WebDriverWait(driver, 5);
+				 waiting.until(ExpectedConditions.elementToBeClickable(marcasWebElements.get(marca)));
+				marcasWebElements.get(marca).click();				
+			}
+		}
+		
 		return new ArrayList<Cafetera>();
+	}
+
+	private static void obtenerMarcasArticulo() {
+		//Click en Ver más para obtener todas las marcas. Usamos findElements para saber si está presente.
+		if(!driver.findElements(By.id("categoryFilterViewMoreBrands")).isEmpty())
+		{ 
+			driver.findElement(By.id("categoryFilterViewMoreBrands")).click();
+		}
+		
+		List<WebElement> elementos = driver.findElements(By.xpath("//*[@id=\"categoryFilterBrands\"]/div/a"));
+		marcasWebElements = new HashMap<String,WebElement>();
+		for(WebElement element : elementos) 
+		{
+			marcasWebElements.put(element.getText(), element);
+		}
 	}
 
 	public static Set<String> getCategorias() {
@@ -27,12 +55,12 @@ public class MediaMarktDriver {
 		List<WebElement> elementosCategoryClass = (List<WebElement>) driver.findElements(By.className("categoryTree"));	
 		categoriasWebElements = new HashMap<String, WebElement>();
 		
-		for(int i=0; i< elementosCategoryClass.size();i++) 
+		for(WebElement element : elementosCategoryClass)
 		{
-			String textoCategoria = elementosCategoryClass.get(i).getText();
+			String textoCategoria = element.getText();
 			
 			if(!categoriasWebElements.containsKey(textoCategoria)) { 
-				categoriasWebElements.put(textoCategoria, elementosCategoryClass.get(i));
+				categoriasWebElements.put(textoCategoria, element);
 			}
 		}				
 		
